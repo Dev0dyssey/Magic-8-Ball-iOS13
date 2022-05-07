85836e2dbd0e26c56f0bdd67eed58b0d00eb6bb4
Thomas Andrew Martin <47917727+Dev0dyssey@users.noreply.github.com>
2022-05-07 15:15:38 +0100

    Added random 8 ball answers

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,8 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    let ballArray = [
+        UIImage(named:"ball1"),
+        UIImage(named:"ball2"),
+        UIImage(named:"ball3"),
+        UIImage(named:"ball4"),
+        UIImage(named:"ball5")
+    ]
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBAction func askButtonPressed(_ sender: Any) {
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
+
 
 
 
